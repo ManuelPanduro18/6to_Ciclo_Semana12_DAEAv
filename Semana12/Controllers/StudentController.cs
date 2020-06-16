e595530fa7fba0a899f48424d25da0db432f85a9
Edson Manuel Manuel Panduro <edson.panduro@tecsup.edu.pe>
2020-06-15 19:32:44 -0500

    Subiendo primer commit

---
+++ Semana12/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Domain;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana12
+{
+    public class StudentController : Controller
+    {
+        private StudentService service = new StudentService();
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Adress = c.studentAdress,
+                             Name = c.studentName
+                         }).ToList();
+
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
---
+++ Semana12/Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Semana12
+{
+    internal class StudentModel
+    {
+        public int ID { get; set; }
+        public string Adress { get; set; }
+        public string Name { get; set; }
+    }
+}
